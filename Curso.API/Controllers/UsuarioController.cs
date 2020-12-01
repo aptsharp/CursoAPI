6886c2d3659bab12086814aa81bb53fcf733faf8
Afonso -aptsharp <aff-on@hotmail.com>
2020-12-01 16:33:51 -0300

    Base do projeto

---
+++ Curso.API/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using Curso.API.Models.Usuarios;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Logar(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+
+    }
+}
---
+++ Curso.API/Models/Usuarios/LoginViewModelInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Curso.API.Models.Usuarios
+{
+    public class LoginViewModelInput
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+    }
+}
