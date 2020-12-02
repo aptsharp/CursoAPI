7be821d96a7cf6d248a091ff5ebfcfc4974eb8c4
Afonso -aptsharp <aff-on@hotmail.com>
2020-12-02 19:11:09 -0300

    Configuração de obrigatoriedade

--- Curso.API/Models/Usuarios/LoginViewModelInput.cs
+++ Curso.API/Models/Usuarios/LoginViewModelInput.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace Curso.API.Models.Usuarios
 {
     public class LoginViewModelInput
     {
+        [Required(ErrorMessage = "O login é obrigatorio")]
         public string Login { get; set; }
+        [Required(ErrorMessage = "A senha é obrigatoria")]
         public string Senha { get; set; }
     }
 }
